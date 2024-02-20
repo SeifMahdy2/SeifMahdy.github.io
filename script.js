230ffc714bcf21ecfae3a63616348b03d1d6b2bd
SEIF\PC <seifwaleedmahdy@gmail.com>
2024-02-20 22:22:39 +0200

    final try

--- script.js
+++
@@ -1,9 +0,0 @@
-function sendMail(){
-      let parms = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            subject: document.getElementById('subject').value,
-            message: document.getElementById('message').value,
-      }
-      emailjs.send("service_4jqjxbu","template_nooygqv", parms).then(alert("Email Sent!"))
-}
