8d77699450a5645584b760ee69520eb11e8326c3
SEIF\PC <seifwaleedmahdy@gmail.com>
2024-02-20 22:06:23 +0200

    new approach emailjs

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-const stripe = Stripe('pk_test_51OlviYBBJNuvkvoKJLj9hFOyszuDnRKQme0tYLb7IwtLrSlUyu8Kz4R1oKsrWj7uDP42VpZ1Ing6PYE1x8M2pGd400PQSaHQdB');
-    const elements = stripe.elements();
-    const cardElement = elements.create('card');
-    cardElement.mount('#card-element');
-
-    document.getElementById('payment-form').addEventListener('submit', async function(event) {
-      event.preventDefault();
-
-      const { token, error } = await stripe.createToken(cardElement);
-
-      if (error) {
-        const errorElement = document.getElementById('card-errors');
-        errorElement.textContent = error.message;
-      } else {
-        // Get the donation amount from the input field
-        const donationAmount = document.getElementById('amount').value;
-        
-        // Open user's email client with prefilled data
-        const donorEmail = document.getElementById('email').value;
-        sendEmail(donorEmail, donationAmount);
-        alert('Thank you for your donation!');
+function sendMail(){
+      let parms = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            subject: document.getElementById('subject').value,
+            message: document.getElementById('message').value,
       }
-    });
-
-    function sendEmail(email, amount) {
-      const subject = encodeURIComponent('Donation Confirmation');
-      const body = encodeURIComponent(`Thank you for your donation of $${amount}.`);
-      const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
-      window.location.href = mailtoLink;
-    }
+      emailjs.send("service_4jqjxbu","template_nooygqv", parms).then(alert("Email Sent!"))
+}
